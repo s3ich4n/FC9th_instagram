fe1a66b05ac5c408b379b206e9f586ef4fb71f73
Seongeun Yu <johong130801@gmail.com>
2018-10-24 23:21:25 +0900

    feat: 로그인, 회원가입 템플릿 및 폼 수정

--- fc-instagram/members/forms.py
+++ fc-instagram/members/forms.py
@@ -1,8 +1,15 @@
 from django import forms
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 이 인스턴스에 주어진 데이터가 유효하면
+        # authenticate에서 리턴된 User 객체를 채움
+        self._user = None
+
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
@@ -18,9 +25,32 @@ class LoginForm(forms.Form):
         )
     )
 
+    def clean(self):
+        super().clean()
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+        user = authenticate(
+            username=username,
+            password=password,
+        )
+        if user is None:
+            raise forms.ValidationError(
+                '사용자명 또는 비밀번호가 올바르지 않습니다.'
+            )
+        self._user = user
+
+    @property
+    def user(self):
+        # 유효성 검증을 실행했을 때
+        # 필드나 폼에서 유효하지 않은 항목이 있으면 이 부분에 추가됨.
+        if self.errors:
+            raise ValueError('폼의 데이터 유효성 검증에 실패하였습니다.')
+        return self._user
+
 
 class RegisterForm(forms.Form):
     username = forms.CharField(
+        label='사용자명',
         widget=forms.TextInput(
             # html 위젯 속성
             # see also: http://getbootstrap.com/docs/4.1/components/forms/#overview
@@ -30,6 +60,7 @@ class RegisterForm(forms.Form):
         )
     )
     password = forms.CharField(
+        label='비밀번호',
         widget=forms.PasswordInput(
             attrs={
                 'class': 'form-control'
@@ -37,6 +68,7 @@ class RegisterForm(forms.Form):
         )
     )
     password_confirm = forms.CharField(
+        label='비밀번호 확인',
         widget=forms.PasswordInput(
             attrs={
                 'class': 'form-control'
@@ -44,11 +76,13 @@ class RegisterForm(forms.Form):
         )
     )
 
-    # clean_<fieldname>() 형태의 메소드
+    # clean_<fildname>() 형태의 메소드
     def clean_username(self):
         data = self.cleaned_data['username']
-        if User.objects.filter(usernam=data).exists():
+        if User.objects.filter(username=data).exists():
+            self.fields['username'].widget.attrs['class'] += 'is-invalid'
             raise forms.ValidationError('이미 사용중인 사용자명 입니다.')
+
         return data
 
     def clean(self):
@@ -56,5 +90,17 @@ class RegisterForm(forms.Form):
         password = self.cleaned_data.get('password')
         password_confirm = self.cleaned_data.get('password_confirm')
         if password != password_confirm:
+            self.fields['password'].widget.attrs['class'] += ' is-invalid'
+            self.fields['password_confirm'].widget.attrs['class'] += ' is-invalid'
             raise forms.ValidationError('비밀번호 값이 서로 일치하지 않습니다.')
 
+        return password_confirm
+
+    def save(self):
+        if self.errors:
+            raise ValueError('폼의 데이터 유효성 검증에 실패했습니다.')
+        user = User.objects.create_user(
+            username=self.cleaned_data['username'],
+            password=self.cleaned_data['password'],
+        )
+        return user
--- fc-instagram/members/views.py
+++ fc-instagram/members/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.template.loader import get_template
 
@@ -9,35 +8,21 @@ from .forms import LoginForm, RegisterForm
 
 # https://docs.djangoproject.com/en/2.1/topics/auth/default/#authenticating-users
 # https://docs.djangoproject.com/en/2.1/topics/auth/default/#how-to-log-a-user-in
-# TODO: 에러처리 수업때 한 소스코드로 공부해오기!
 def login_view(request):
     template = get_template('members/login.html')
-    form = LoginForm()
-    context = {
-        'form': form,
-    }
+    context = {}
 
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(
-            request,
-            username=username,
-            password=password,
-        )
-        if user is not None:
-            login(request, user)
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            login(request, form.user)
             return redirect('posts:post_list')
-        else:
-            context['error'] = 'error'
-            return HttpResponse(template.render(context, request))
-
     else:
-        context = {
-            'form': form,
-        }
+        form = LoginForm()
 
-        return HttpResponse(template.render(context, request))
+    context['form'] = form
+    return HttpResponse(template.render(context, request))
 
 
 def logout_view(request):
@@ -50,18 +35,16 @@ def register_view(request):
     template = get_template('members/register.html')
     context = {}
 
-    # TODO: 입력값에 대한 유효성 검증
     if request.method == 'POST':
         form = RegisterForm(request.POST)
 
         if form.is_valid():
-            user = User.objects.create_user(
-                username=form.cleaned_data['username'],
-                password=form.cleaned_data['password'],
-            )
+            user = form.save()
             login(request, user)
             return redirect('posts:post_list')
     else:
         form = RegisterForm()
-        context['form'] = form
-        return HttpResponse(template.render(context, request))
+
+    # form 렌더링은 이런식으로
+    context['form'] = form
+    return HttpResponse(template.render(context, request))

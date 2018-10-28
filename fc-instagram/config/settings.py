0a726ad5ebabf03329bb1083cb057bfdb83820cd
Seongeun Yu <johong130801@gmail.com>
2018-10-28 19:22:51 +0900

    feat: UserModel 커스터마이징

--- fc-instagram/config/settings.py
+++ fc-instagram/config/settings.py
@@ -51,6 +51,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Auth
+AUTH_USER_MODEL = 'members.User'
 
 # Application definition
 
--- fc-instagram/members/models.py
+++ fc-instagram/members/models.py
@@ -1,40 +1,27 @@
-# from django.db import models
-#
-#
-# class User(models.Model):
-#     # profile_image = models
-#     username = models.CharField(
-#         '아이디',
-#         max_length=32,
-#     )
-#     img_profile = models.ImageField(
-#         '프로필 이미지',
-#         upload_to='user',
-#         blank=True,
-#     )
-#     name = models.CharField(
-#         '이름',
-#         max_length=32,
-#         blank=True,
-#     )
-#     website = models.CharField(
-#         '웹사이트',
-#         max_length=64,
-#         blank=True,
-#     )
-#     bio_info = models.TextField(
-#         '소개',
-#         blank=True,
-#         max_length=64,
-#     )
-#     # User = models.ManyToManyField(
-#     #     'self',
-#     #
-#     # )
-#
-#     def __str__(self):
-#         return self.name
-#
-#     class Meta:
-#         verbose_name = '사용자'
-#         verbose_name_plural = '사용자 목록'
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    img_profile = models.ImageField(
+        '프로필 이미지',
+        upload_to='user',
+        blank=True,
+    )
+    website = models.CharField(
+        '웹사이트',
+        max_length=128,
+        blank=True,
+    )
+    bio_info = models.TextField(
+        '소개',
+        blank=True,
+        max_length=64,
+    )
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = '사용자'
+        verbose_name_plural = '사용자 목록'
